6c2992ae758e1ecfdfbcc45321f7c1430f90463d
mattbzca <111474919+mattbzca@users.noreply.github.com>
2022-11-02 20:44:15 -0700

    Updated script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+//#1 
+let myGrade = 97;
+//#2
+if(myGrade >= 90) {
+    console.log("A");
+} else if (myGrade = [ 80, 81, 82, 83, 84, 85, 86, 87, 88, 89 ]) {
+console.log("B");
+} else if (myGrade = [ 70, 71, 72, 73, 74, 75, 76, 77, 78, 79 ]) {
+    console.log("C");
+} else if (myGrade = [ 60, 61, 62, 63, 64, 65, 66, 67, 68, 69 ]) {
+    console.log("D");
+} else if (myGrade <= 59) {
+    console.log("F");
+    }
+//#3
+
+//#4
+for (let i = 1; i <=10; i++); {
+    console.log(i);
+}
+
+
---
+++ scripttest.js
@@ -0,0 +1,34 @@
+// if-else statement
+if (inputNumber % 2 == 0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
+//nested if statement
+if () {
+    console.log();
+    if () {
+        console.log();
+    }
+} else {
+    console.log();
+}
+
+
+let myRating = 1 
+
+//switch statements for rating
+switch(myRating) {
+    case 1:
+        console.log("You get the highest rating!");
+        break;
+    case 2:
+        console.log("You get an average rating!");
+    case 3:
+        console.log("You get a low rating!");
+}
+
+//for loop statement
+for (let i = 0; 1 < 3; i++) {
+    console.log(1);
+}
